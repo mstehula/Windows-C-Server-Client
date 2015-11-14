86a61e22573bd62cfded33a1c821c2c3861f0814
mstehula <mstehula@gmail.com>
2015-11-14 10:14:08 -0600

    Added simple main with hello world in it

---
+++ client/client.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+// -
+// Func: This is the main thread for the client executable
+// Args: Any command line arugments for the clinet to observe
+// Retn: Errors, if any. Else, will return a 0
+// -
+{
+  printf("Hello World!\n");
+  return(0);
+}
