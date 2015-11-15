b43fe846c09dad222a9c30842dc66b3d7c08eeb2
mstehula <mstehula@gmail.com>
2015-11-14 19:05:07 -0600

    Added command line interfaces when calling the executable

--- client/client.c
+++ client/client.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 
-int main()
+int main(int argc, char * argv[])
 // -
 // Func: This is the main thread for the client executable
 // Args: Any command line arugments for the clinet to observe
 // Retn: Errors, if any. Else, will return a 0
 // -
 {
-  printf("Hello World!\n");
+
+  if(argc == 1)
+  // No arguments
+  {
+
+  }
+  else
+  // More than one arguments
+  {
+
+  }
+
   return(0);
 }
