0e5d279ca35047d4afd207b8b1f7bc472e6f6c7c
mstehula <mstehula@gmail.com>
2015-11-19 17:14:29 -0500

    Added client.h and main.c to go with previous commit

---
+++ client/client.h
@@ -0,0 +1,9 @@
+
+#ifndef CLIENT_H_
+#define CLIENT_H_
+
+struct settings {
+  int shutdown_after_start = 0;
+}
+
+#endif //CLIENT_H_
---
+++ client/main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(int argc, char * argv[])
+// -
+// Func: This is the main thread for the client executable
+// Args: Any command line arugments for the clinet to observe
+// Retn: Errors, if any. Else, will return a 0
+// -
+{
+
+  if(argc == 1)
+  // No arguments
+  {
+
+  }
+  else
+  // More than one arguments
+  {
+    for(; argc > 0; argc--)
+    //Loop through all arguments
+    {
+
+    }
+  }
+
+  return(0);
+}
