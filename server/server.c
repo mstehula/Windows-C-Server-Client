507ca282786a5599088c67aa26239a9dcc136869
mstehula <mstehula@gmail.com>
2015-11-21 09:32:23 -0600

    Changed stuff around again

--- server/server.c
+++ server/server.c
@@ -1,9 +1,31 @@
+#include <stdio.h>
 #include "server.h"
 
-int start_server(struct settings) {
+int main(int argc, char * argv[])
+// -
+// Func: This is the main thread for the client executable
+// Args: Any command line arugments for the clinet to observe
+// Retn: Errors, if any. Else, will return a 0
+// -
+{
+  if(argc == 1)
+  // No arguments
+  {
 
-}
+  }
+  else
+  // More than one arguments
+  {
+    for(; argc > 0; argc--)
+    //Loop through all arguments
+    {
 
-void stop_server(int server_id) {
-  
+    }
+  }
+
+  int server_id = start_server();
+
+  stop_server(server_id);
+
+  return(0);
 }
